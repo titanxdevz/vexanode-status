542d97da1b55c038792647a4b26af141279ce673
titanxdevz <titanxdevz@users.noreply.github.com>
2026-06-09 12:50:39 -0500

    update panel URLs and remove omega node

--- src/config/monitors.ts
+++ src/config/monitors.ts
@@ -1,8 +1,6 @@
 export const MONITORS = [
   { name: 'Main Website', url: 'https://vexanode.cloud/' },
-  { name: 'Panel', url: 'https://panel.vexanode.cloud/' },
+  { name: 'Game Panel', url: 'https://panel.vexanode.gg/' },
   { name: 'VPS Nodes', url: 'https://vps.vexanode.cloud/' },
-  { name: 'Billing Panel', url: 'https://billing.vexanode.cloud/' },
-  { name: 'Omega Node', url: 'https://omega.vexanode.cloud/' },
-
+  { name: 'Billing Panel', url: 'https://billing.vexanode.gg/' },
 ];
